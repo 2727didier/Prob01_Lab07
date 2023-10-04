b00ea51f6c0671f0671dde7137423bee0616e385
USUARIO <USUARIO@DESKTOP-Q5ALF4J>
2023-10-03 19:12:21 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob01_Lab07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double resultado = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                resultado = 1.0 / i;
+                Console.WriteLine($"resultado : {resultado:F7}");
+            }
+
+            resultado = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                resultado += 1.0 / i;
+            }
+
+            Console.WriteLine(resultado);
+        }
+    }
+
+}
+    
